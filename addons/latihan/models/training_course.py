2340f3e95792dbafeb453040d313b986265783a1
silkiy <wildanmilk@gmail.com>
2024-08-29 11:32:22 +0700

    day 2

--- addons/latihan/models/training_course.py
+++ addons/latihan/models/training_course.py
@@ -9,26 +9,39 @@ class TrainingCourse(models.Model):
     user_id = fields.Many2one(comodel_name='res.users', string='Penanggung Jawab')
     session_line = fields.One2many(comodel_name='training.session', inverse_name='course_id', string='Sessions')
     instruktur_id = fields.Many2one(comodel_name='res.partner', string='Instruktur')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirmed'),
+        ('done', 'Done'),
+    ], string='Status', default='draft', tracking=True)
+
 
 class TrainingSession(models.Model):
     _name = 'training.session'
     _description = 'Training Session'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string='Session Name', required=True)
-    course_id = fields.Many2one(comodel_name='training.course', string='Course Name', required=True, ondelete='cascade')
-    start_date = fields.Date(string='Start Date', required=True)
-    duration = fields.Float(string='Duration', required=True)
-    seats = fields.Integer(string='Seats', required=True)
-    peserta_ids = fields.Many2many(comodel_name='peserta', string='Peserta')
-    instruktur_id = fields.Many2one(comodel_name='res.partner', string='Instruktur')
+    name = fields.Char(string='Session Name', required=True, tracking=True) 
+    course_id = fields.Many2one(comodel_name='training.course', string='Course Name', required=True, ondelete='cascade', tracking=True)
+    start_date = fields.Date(string='Start Date', required=True , tracking=True)
+    duration = fields.Float(string='Duration', required=True, tracking=True)
+    seats = fields.Integer(string='Seats', required=True, tracking=True)
+    peserta_ids = fields.Many2many(comodel_name='peserta', string='Peserta', tracking=True)
+    instruktur_id = fields.Many2one(comodel_name='res.partner', string='Instruktur', tracking=True)
     
-    no_hp = fields.Char(string='No HP', related='instruktur_id.mobile')
-    email = fields.Char(string='Email', related='instruktur_id.email')
-    jenis_kelamin = fields.Selection(related='instruktur_id.jenis_kelamin')
+    
+    no_hp = fields.Char(string='No HP', related='instruktur_id.mobile', tracking=True)
+    email = fields.Char(string='Email', related='instruktur_id.email', tracking=True)
+    jenis_kelamin = fields.Selection(related='instruktur_id.jenis_kelamin', tracking=True)
 
-    state = fields.Selection(string='Status', selection=[('draft', 'Draft'), ('confirm', 'Sedang Berlangsung'), ('done', 'Selesai')], default='draft', tracking=True)
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirmed'),
+        ('done', 'Done'),
+    ], string='Status', default='draft', tracking=True)
 
-    jml_peserta = fields.Integer(string='Jumlah Peserta', compute='_compute_jml_peserta')
+
+    jml_peserta = fields.Integer(string='Jumlah Peserta', compute='_compute_jml_peserta', tracking=True)
 
     @api.depends('peserta_ids')
     def _compute_jml_peserta(self):
